886b666bf46efc95379385d55dda9955ff80ccff
Chanuka Mullevidana <chanukadil1999@gmail.com>
2025-03-09 19:36:16 +0530

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Chanuka Dilhara on 2025-03-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
